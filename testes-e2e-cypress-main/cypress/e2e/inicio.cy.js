ff5111e8101b07c177103f99d0dc77b0c0c70ce3
JuliaValadares <ju.valadares12@gmail.com>
2024-08-16 15:19:30 -0300

    testes

--- testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -7,4 +7,33 @@ describe('Página Principal', () => {
     cy.visit('http://localhost:3000')
     cy.getByData('titulo-principal')
   })
+  
+  it('Deve renderizar h2 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('vantagens').contains('Vantagens do nosso banco')
+  })
+  
+  it('Deve renderizar h3 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('cartao').contains('Conta e cartão gratuitos')
+  })
+
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('paragrafo').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+  })
+
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('sacar').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
+  })
+
+  it('Deve renderizar h3 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('pontos').contains('Programa de pontos')
+  })
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('pontos').contains('Programa de pontos')
+})
 })
--- testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
+++ testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
@@ -12,22 +12,22 @@ export default function Inicio() {
       <div className={estilos.container}>
         <div className={estilos.conteudo}>
           <div className={estilos.banner}>
-            <h3 data-test="titulo-principal">
+            <h1 data-test="titulo-principal">
               Experimente mais liberdade no controle da sua vida financeira.
               Crie sua conta com a gente!
-            </h3>
+            </h1>
             <img
               src={ilustracaoBanner}
               alt="Gráficos e uma pessoa com dinheiro na mão"
             />
           </div>
           <div className={estilos.vantagens}>
-            <h2>Vantagens do nosso banco:</h2>
+            <h2 data-test="vantagens">Vantagens do nosso banco:</h2>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
                 <img src={presente} alt="ícone de um presente" />
-                <h3>Conta e cartão gratuitos</h3>
-                <p>
+                <h3 data-test="cartao">Conta e cartão gratuitos</h3>
+                <p data-test="paragrafo">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
                 </p>
@@ -35,15 +35,15 @@ export default function Inicio() {
               <div className={estilos.vantagens__item}>
                 <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
                 <h3>Saques sem custo</h3>
-                <p>
+                <p data-test="sacar">
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
                   24h.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
                 <img src={estrela} alt="ícone de uma estrela" />
-                <h3>Programa de pontos</h3>
-                <p>
+                <h3 data-test="pontos">Programa de pontos</h3>
+                <p data-test="acumular">
                   Você pode acumular pontos com suas compras no crédito sem
                   pagar mensalidade!
                 </p>
